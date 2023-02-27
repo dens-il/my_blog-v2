789b43341e87dadfdc6f3e22d97ec178f3ac12d4
dens-il <126162616+dens-il@users.noreply.github.com>
2023-02-27 14:13:13 +0200

    Add files via upload

---
+++ js/core.js
@@ -0,0 +1,57 @@
+document.querySelector('.main__block3-txt').addEventListener('click', function(){
+	this.textContent = this.textContent == "Author of the site: student of the 1KI2 group Denys Oleksandrovych Alimov. The site itself was created with the aim of strengthening theoretical knowledge in practice. The site did not receive cool technologies or unusual visual effects. The following languages were used to create the site: HTML, CSS, JS." ? "Автор сайту: студент групи 1КІ2 Алімов Денис Олександрович. Сам сайт створений з ціллю укріпити тереотичні знання на практиці, тому сайт не отримав крутих технологій чи незвичайних візуальних ефектів. Для створення сайту були задіяні такі мови: HTML, CSS, JS." : "Author of the site: student of the 1KI2 group Denys Oleksandrovych Alimov. The site itself was created with the aim of strengthening theoretical knowledge in practice. The site did not receive cool technologies or unusual visual effects. The following languages were used to create the site: HTML, CSS, JS.";
+});
+document.querySelector('.myClass').addEventListener('click', function(){
+	this.textContent = this.textContent == 'What does the site consist of?' ? 'З чого складається сайт?' : 'What does the site consist of?';
+});
+document.querySelector('.myClass3').addEventListener('click', function(){
+	this.textContent = this.textContent == 'The site involved HTML files.' ? "На сайті використовуються файли HTML." : 'The site involved HTML files.';
+});
+document.querySelector('.myClass4').addEventListener('click', function(){
+	this.textContent = this.textContent == 'The site involved CSS files.' ? "На сайті використовуються файли CSS." : 'The site involved CSS files.';
+});
+document.querySelector('.myClass5').addEventListener('click', function(){
+	this.textContent = this.textContent == 'The site involved JS files.' ? "На сайті використовуються файли JS." : 'The site involved JS files.';
+});
+document.querySelector('.myClass6').addEventListener('click', function(){
+	this.textContent = this.textContent == 'The site weighs about 5 MB.' ? "Сайт важить близько 5 мб." : 'The site weighs about 5 MB.';
+});
+document.querySelector('.myClass7').addEventListener('click', function(){
+	this.textContent = this.textContent == 'It took about 4 days to develop the site.(actually 10)' ? "На розробку сайту пішло близько 4 дня.(насправді10)" : 'It took about 4 days to develop the site.(actually 10)';
+});
+document.querySelector('.myClass8').addEventListener('click', function(){
+	this.textContent = this.textContent == 'The site was created by myself at no cost.' ? "Сайт був створений власноруч без затрат." : 'The site was created by myself at no cost.';
+});
+document.querySelector('.myClass22').addEventListener('click', function(){
+	this.textContent = this.textContent == 'Hello, my name is Denis and I am 16 years old. I come from Ukraine. I am studying in the first year of college.' ? "Привіт, мене звати Денис і мені 16 років. Я родом з України. Навчаюся на першому курсі коледжу." : 'Hello, my name is Denis and I am 16 years old. I come from Ukraine. I am studying in the first year of college.';
+});
+document.querySelector('.block-poloski-text1').addEventListener('click', function(){
+	this.textContent = this.textContent == 'About that many months I learned to program.' ? "Приблизно стільки місяців я навчаюся програмуванню." : 'About that many months I learned to program.';
+});
+document.querySelector('.block-poloski-text2').addEventListener('click', function(){
+	this.textContent = this.textContent == 'This is a lesson in the programming course that I stopped at.' ? "Це урок з курсу програмування, на якому я зупинився." : 'This is a lesson in the programming course that I stopped at.';
+});
+document.querySelector('.block-poloski-text3').addEventListener('click', function(){
+	this.textContent = this.textContent == "That's how many Java Script skills I have." ? "Саме стільки я маю навичок Java Script." : "That's how many Java Script skills I have.";
+});
+document.querySelector('.block-poloski-text4').addEventListener('click', function(){
+	this.textContent = this.textContent == 'The number of files I used to create the site.' ? "Кількість файлів, які я використав для створення сайту." : 'The number of files I used to create the site.';
+});
+document.querySelector('.block-poloski-text5').addEventListener('click', function(){
+	this.textContent = this.textContent == 'Number of hours won by me in CS:GO.' ? "Кількість годин, награних мною в CS:GO." : 'Number of hours won by me in CS:GO.';
+});
+document.querySelector('.footer__quote-text').addEventListener('click', function(){
+	this.textContent = this.textContent == 'The real hero, glorious, is not the one in shining armor and a beautiful pose. A hero is someone who does what needs to be done. And you have to get dirty, and shed blood, but he still does it, because it’s necessary. A man is a man because he understands this.' ? 'Настоящий герой, Слав, это не тот, кто в сияющих доспехах и красивой позе. Герой — тот, кто делает, что нужно сделать. И запачкаться приходиться, и кровь проливать, а он всё равно делает, потому что так надо. Мужик — он потому и мужик, что это понимает.' : 'The real hero, glorious, is not the one in shining armor and a beautiful pose. A hero is someone who does what needs to be done. And you have to get dirty, and shed blood, but he still does it, because it’s necessary. A man is a man because he understands this.';
+});
+document.querySelector('.translete1').addEventListener('click', function(){
+	this.textContent = this.textContent == 'About me' ? "Про мене" : 'About me';
+});
+document.querySelector('.translete2').addEventListener('click', function(){
+	this.textContent = this.textContent == 'Information' ? "Інформація" : 'Information';
+});
+
+
+
+
+
+
